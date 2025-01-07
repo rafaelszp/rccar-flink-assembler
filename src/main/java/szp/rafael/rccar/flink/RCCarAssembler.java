b27874f6fce0126108bd6c1f47ed4f9382423a60
Rafael Pereira <rafael.pereira@cilia.com.br>
2025-01-07 18:51:38 -0300

    wip: rest api + kafka sink

--- src/main/java/szp/rafael/rccar/flink/RCCarAssembler.java
+++ src/main/java/szp/rafael/rccar/flink/RCCarAssembler.java
@@ -1,21 +1,20 @@
 package szp.rafael.rccar.flink;
 
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.configuration.CheckpointingOptions;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.file.sink.FileSink;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
-import szp.rafael.rccar.dto.Body;
-import szp.rafael.rccar.dto.Engine;
 import szp.rafael.rccar.dto.RCCar;
-import szp.rafael.rccar.dto.RemoteControl;
 import szp.rafael.rccar.flink.stream.BodyEngineJoin;
 import szp.rafael.rccar.flink.stream.RCCarRemoteControlJoin;
 import szp.rafael.rccar.flink.stream.RCCarWheelsJoin;
 import szp.rafael.rccar.flink.util.RCCarStreamFactory;
+import org.apache.flink.streaming.api.CheckpointingMode;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class RCCarAssembler {
@@ -23,6 +22,19 @@ public class RCCarAssembler {
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(1000*600); // Checkpoint a cada 60 segundos
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE); // Modo de checkpoint
+
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(500); // Pausa mínima entre checkpoints
+        env.getCheckpointConfig().setCheckpointTimeout(1000*5*60); // Timeout de checkpoint
+        env.getCheckpointConfig().setTolerableCheckpointFailureNumber(2);
+        // sets the checkpoint storage where checkpoint snapshots will be written
+        Configuration config = new Configuration();
+        config.set(CheckpointingOptions.CHECKPOINT_STORAGE, "filesystem");
+        config.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "file:///"+System.getProperty("java.io.tmpdir")+"/checkpoints");
+        config.set(CheckpointingOptions.ENABLE_CHECKPOINTS_AFTER_TASKS_FINISH, true);
+        env.configure(config);
+
 
         var bodyStream = RCCarStreamFactory.createBodyStream(env);
         var engineStream = RCCarStreamFactory.createEngineStream(env);
